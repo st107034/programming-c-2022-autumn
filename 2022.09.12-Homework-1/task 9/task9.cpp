992455fbd40c5d1d56a4612be79627482e3e59bb
st107034 <114332076+st107034@users.noreply.github.com>
2022-09-25 22:51:05 +0300

    tasks

--- 2022.09.12-Homework-1/task 9/task9.cpp
+++ 2022.09.12-Homework-1/task 9/task9.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
     int res=0;
     std::cin>>x;
     x2=x*x;
-    res=x2*(x2+x)+x2+x+1;
+    res=(x2+x)*(x2+1)+1;
     std::cout <<res<<std::endl;
     return EXIT_SUCCESS;
 }
