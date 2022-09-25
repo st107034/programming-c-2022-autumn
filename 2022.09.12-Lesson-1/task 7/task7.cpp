e27db6cf3961974c81010dfe8394052c43255970
st107034 <114332076+st107034@users.noreply.github.com>
2022-09-25 21:43:21 +0300

    Create task7.cpp

---
+++ 2022.09.12-Lesson-1/task 7/task7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    int a=0;
+    int b=0;
+    int c=0;
+    std::cin>>a;
+    std::cin>>b;
+    c=b;
+    b=a;
+    a=c;
+    std::cout<<a<<" "<<b<<std::endl;
+    return EXIT_SUCCESS;
+}
