9fc1bc6e379fbf4db5afc63c2dd87e0db9b09222
st107034 <114332076+st107034@users.noreply.github.com>
2022-09-25 14:31:45 +0300

    task1

---
+++ 2022.09.12-Lesson 1/task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int b;
+    cin>>b;
+    int a = b - 1;
+    int c = b + 1;
+    cout << "The next number for the number "<<b<<" is "<<c<<"."<<endl;
+    cout << "The previuos number for the number "<<b<<" is "<<a<<"."<<endl;
+    return 0;
+}
