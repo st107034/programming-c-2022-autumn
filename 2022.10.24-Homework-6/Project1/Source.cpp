d7fe356ec21b0da3531c0fb2930412d4a33b1a3b
st107034 <114332076+st107034@users.noreply.github.com>
2022-12-18 15:40:47 +0600

    fix

--- 2022.10.24-Homework-6/Project1/Source.cpp
+++ 2022.10.24-Homework-6/Project1/Source.cpp
@@ -4,6 +4,7 @@ int main(int argc, char* argv[])
 {
 	const int N = 1000;
 	int n = 0;
+	int b = 0;
 	int a[N]{ 0 };
 
 	std::cin >> n;
@@ -13,7 +14,14 @@ int main(int argc, char* argv[])
 		std::cin >> a[i];
 	}
 
-	for (int i = n - 1; i >= 0; i--)
+	for (int i = 0; i < (n / 2); i++)
+	{
+		b = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = b;
+	}
+
+	for (int i = 0; i < n; i++)
 	{
 		std::cout << a[i] << " ";
 	}
