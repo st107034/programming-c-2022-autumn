c9d16bb98adfedc9fd7beffefe81ada613fdc2b6
st107034 <114332076+st107034@users.noreply.github.com>
2022-12-18 15:28:19 +0600

    fix

--- 2022.10.17-Homework-5/Project3/Source.cpp
+++ 2022.10.17-Homework-5/Project3/Source.cpp
@@ -13,12 +13,18 @@ int main(int argc, char* argv[])
 	for (int i = 0; i < n; i++)
 	{
 		std::cin >> a[i];
+	}
 
+	for (int i = 0; i < n; i++)
+	{
 		if (a[i] > mx)
 		{
 			mx = a[i];
 		}
+	}
 
+	for (int i = 0; i < n; i++)
+	{
 		if (a[i] < mn)
 		{
 			mn = a[i];
